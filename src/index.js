cb2820c3fbe9dfde2899c24b72be1592a974652d
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2018-12-03 12:42:43 -0300

    botones

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-/* Acá va tu código */
+//window.onload
+
+function cifrarTexto(){
+    const root= document.getElementById("root");
+    let offset = document.getElementById("offset").value;
+    let mensaje= document.getElementById("introtext").value;
+    root.innerHTML = window.cipher.encode(offset, mensaje);
+    }
+    
+    function descifrarTexto(){
+        const root= document.getElementById("root");
+        let offset = document.getElementById("offset").value;
+        let mensaje= document.getElementById("introtext").value;
+        root.innerHTML = window.cipher.decode(offset, mensaje);
+    }
