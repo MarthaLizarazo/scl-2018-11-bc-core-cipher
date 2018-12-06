dcbc39cce5ebe48c4d9b959646b620df7f1f633a
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2018-12-06 12:48:37 -0300

    Probando Funciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,33 @@
 window.cipher = {
   encode: (offset,mensaje) => {
   let resultado="";
-  offset=parseInt(offset); // el parseInt trae el valor como numero, no como string
+  offset=parseInt(offset);                                          // el parseInt trae el valor como numero, no como string
   for (let i= 0; i<mensaje.length; i++) {
     let letraAscci = mensaje.charCodeAt(i);
+
     if (letraAscci >= 65 && letraAscci<= 90){
      resultado += String.fromCharCode((letraAscci-65+offset)%26+65) // el += es para traerme la cadena completa de texto
-    }else {
-   resultado += String.fromCharCode(letraAscci);
-
-    if (letraAscci>=97 && letraAscci<=122) { // condición código ASCII (Valores minúsculas)
-    resultado += String.fromCharCode((letraAscii-97+offset)%26+97) //Obtener el nuevo código ASCII mediante fórmula
+    }else if (letraAscci>=97 && letraAscci<=122) {                  // condición código ASCII (Valores minúsculas)
+     resultado += String.fromCharCode((letraAscci-97+offset)%26+97) //Obtener el nuevo código ASCII mediante fórmula
     }else{
-
-    }
     resultado += String.fromCharCode(letraAscci);
     }
   }
-  
 
   return resultado;
 
   },
   decode: (offset,mensaje) => {
   let resultado="";
-  offset=parseInt(offset); // el parseInt trae el valor como numero, no como string
+  offset=parseInt(offset);                                          // el parseInt trae el valor como numero, no como string
   for (let i= 0; i<mensaje.length; i++) {
     let letraAscci = mensaje.charCodeAt(i);
+
     if (letraAscci >= 65 && letraAscci<= 90){
-     resultado += String.fromCharCode((letraAscci-65-offset)%26+65); // el += es para traerme la cadena completa de texto
-    }else {
+     resultado += String.fromCharCode((letraAscci-65-offset)%26+65) // el += es para traerme la cadena completa de texto
+    }else if (letraAscci>=97 && letraAscci<=122) {                  // condición código ASCII (Valores minúsculas)
+     resultado += String.fromCharCode((letraAscci-97-offset)%26+97)
+    }else{
      resultado += String.fromCharCode(letraAscci);
     }
   }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-//window.onload
 
-function cifrarTexto(){
-    const root= document.getElementById("root");
-    let offset = document.getElementById("offset").value;
-    let mensaje= document.getElementById("introtext").value;
-    root.innerHTML = window.cipher.encode(offset, mensaje);
-    }
+window.onload = ()=>{
+
+const boton1=document.getElementById("cifrarTexto"); //get= consedguir y guarda un elemento con el Id asignado
+
+boton1.addEventListener("click", () =>{   //Cuando se hace "click" llama a la funcion listener=escucha, los () significa que es una funcion
+        const root= document.getElementById("root");
+        let offset = document.getElementById("offset").value;
+        let mensaje= document.getElementById("introtext").value;
+        root.innerHTML = window.cipher.encode(offset, mensaje);
+    });
+
     
-    function descifrarTexto(){
+const boton2=document.getElementById("descifrarTexto");
+
+boton2.addEventListener("click", () =>{
         const root= document.getElementById("root");
         let offset = document.getElementById("offset").value;
         let mensaje= document.getElementById("introtext").value;
         root.innerHTML = window.cipher.decode(offset, mensaje);
-    }
+    });
+}
